f751f3e976fb239340757f76c2bdc9149042de5c
joacoorqueda <101809787+joacoorqueda@users.noreply.github.com>
2022-08-19 10:44:01 -0300

    GuiaProgramacion Joaquin.O

---
+++ Assets/Scripts/Ej1Puntajes.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1Puntajes : MonoBehaviour
+{
+    public int[] puntajes = new int [10];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(ObtenerMayorPuntaje());
+        Debug.Log(ObtenerMenorPuntaje());
+        Debug.Log(ObtenerPuntajePromedio());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int ObtenerMayorPuntaje()
+    {
+
+        int MayorPuntaje = 0;
+
+        foreach (int puntaje in puntajes)
+        {
+            if(puntaje > MayorPuntaje)
+            {
+                MayorPuntaje = puntaje;
+            }
+        }
+        return MayorPuntaje;
+    }
+
+
+    int ObtenerMenorPuntaje()
+    {
+        int ObtenerMenorPuntajes = 0;
+        int menorPuntaje = puntajes[0];
+        for(int i= 0; i<puntajes.Length; i++)
+        {
+            if(puntajes[i] < menorPuntaje)
+            {
+                menorPuntaje = puntajes[i];
+                ObtenerMenorPuntajes = i;
+            }
+
+        }
+        return ObtenerMenorPuntajes;
+    }
+
+    float ObtenerPuntajePromedio()
+    {
+
+        float promedio = 0;
+
+        float suma = 0;
+        foreach(int num in puntajes)
+        {
+            suma += num;
+        }
+        promedio = suma / puntajes.Length;
+
+        return promedio;
+    }
+}
---
+++ Assets/Scripts/Ej2MesesyDiasdelAño.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2MesesyDiasdelAño : MonoBehaviour
+{
+
+    [SerializeField] string[] nombresmeses = new string[12];
+    [SerializeField] int[] diasporcadames = new int[12];
+    public string FechaIngresada;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ValidarFormatoFecha(FechaIngresada);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool ValidarFormatoFecha(string fecha)
+    {
+        bool Fechavalida = false;
+
+        if (fecha.Length == 4)
+        {
+            string dia = "";
+            string mes = "";
+
+            dia += fecha[0];
+
+            dia += fecha[1];
+
+            int diaNumber = int.Parse(dia);
+
+            Debug.Log(dia);
+
+            mes += fecha[2];
+            mes += fecha[3];
+            int mesNumber = int.Parse(mes);
+
+            if(diaNumber >0 && diaNumber<=31 && mesNumber >0 && mesNumber <= 12)
+            {
+                Fechavalida = true;
+            }
+        }
+        else
+        {
+            Debug.Log("El largo de la fecha es incorrecto");
+        }
+
+        return Fechavalida;
+    }
+}
